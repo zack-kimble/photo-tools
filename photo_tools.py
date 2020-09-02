9dca2e7915e37472908661abcf8dbed05aa0b9e6
zkimble <zkimble@gmail.com>
2020-09-02 04:31:08 -0400

    cleanup

--- photo_tools.py
+++ photo_tools.py
@@ -8,10 +8,14 @@ Created on Sun May 17 10:07:19 2020
 import datetime
 import json
 import os
+import re
+import shutil
+
 
 import exiftool
 import pandas as pd
 import dask
+import numpy as np
 
 from dask.distributed import Client
 
@@ -198,50 +202,111 @@ def create_id_df(meta_df):
     id_df = meta_df[['time_id', 'file_prefix']].copy()
     id_df = id_df.drop_duplicates(subset=['time_id','file_prefix'])
     #id_df['file_prefix'] = id_df['filepath'].apply(lambda x: x.split('.')[0])
+    id_df = id_df.set_index(['time_id', 'file_prefix'])
     return id_df
 
 def create_merged_meta_df(meta_df):
     #meta_df = pd.read_pickle('meta_df.pkl')
     merged_meta_df = meta_df.copy()
-    merged_meta_df['files_with_key'] = merged_meta_df.groupby(['time_id', 'metadata_key', 'metadata_type'])['filepath'].transform('count')
+    merged_meta_df['files_with_key'] = merged_meta_df.groupby(['time_id','file_prefix', 'metadata_key', 'metadata_type'])['filepath'].transform('count')
     merged_meta_df.loc[merged_meta_df.files_with_key >1, 'source_file_type']  = 'multi'
     merged_meta_df.loc[merged_meta_df.files_with_key ==1, 'source_file_type' ] = merged_meta_df.loc[merged_meta_df.files_with_key ==1, 'file_suffix']
     missing_time_id = merged_meta_df[merged_meta_df.source_file_type.isna()]
     merged_meta_df = (merged_meta_df.dropna(subset=['time_id'])\
-        .drop_duplicates(subset=['time_id', 'metadata_key', 'metadata_type','source_file_type'])
+        .drop_duplicates(subset=['time_id', 'file_prefix', 'metadata_key', 'metadata_type','source_file_type'])
         .drop(columns=['filepath','file_suffix'])
-        .set_index(['time_id','metadata_type','metadata_key'])
+        .set_index(['time_id','file_prefix','metadata_type','metadata_key'])
         )
     return merged_meta_df
 
 
-def convert_filepath_to_kws(meta_id_df, original_photo_dirs):
-    relevant_path_structure = meta_id_df.set_index('time_id').file_prefix.copy()
+def convert_filepath_to_kwds(meta_id_df, original_photo_dirs):
+    relevant_path_structure = meta_id_df.reset_index().copy()
+    relevant_path_structure['new_kwds'] = relevant_path_structure['file_prefix']
     for directory in original_photo_dirs:
-        relevant_path_structure = relevant_path_structure.str.replace(directory,'')
-    new_kws = relevant_path_structure.apply(lambda x: x.split('.')[:-1])
-    return new_kws
+        relevant_path_structure['new_kwds'] = relevant_path_structure.new_kwds.str.replace(directory,'',regex=False)
+    relevant_path_structure = relevant_path_structure.reset_index().set_index(['time_id','file_prefix'])
+    new_kwds = relevant_path_structure.new_kwds.apply(lambda x: x.strip('/').split('/')[:-1])
+    return new_kwds , relevant_path_structure
 
-def extend_kws(series):
+def extend_kwds(series):
     current = series['metadata_value']
+    new = list(series['new_kwds'])
+    if current != current:
+        return new
     if type(current) == str:
         current = [current]
-    new = list(series['new_kws'])
     current.extend(new)
     return current
 
-def add_kws_to_df(kws, meta_df):
-    kws = kws.rename('new_kws')
-    kw_metadata = meta_df.query("metadata_key in ['Subject','HierarchicalSubject']").copy()
-    kw_metadata = kw_metadata.reset_index(['metadata_type', 'metadata_key']).join(kws)
-    kw_metadata['metadata_value'] = kw_metadata.apply(extend_kws, axis=1)
-    kw_metadata = kw_metadata.reset_index().set_index(['time_id','metadata_type','metadata_key'])
-    meta_df.update(kw_metadata)
-    return meta_df
+
+# def add_kwds_to_df(kwds, meta_df):    
+#     kwds['metadata_key'] = 'Subject'
+#     kwd_metadata = meta_df.query("metadata_key in ['Subject','HierarchicalSubject']").copy()
+#     kwd_metadata = kwd_metadata.reset_index(['metadata_type', 'metadata_key']).join(kwds, how='outer')
+#     kwd_metadata['metadata_value'] = kwd_metadata.apply(extend_kwds, axis=1)
+#     kwd_metadata = kwd_metadata.reset_index().set_index(['time_id','metadata_type','metadata_key'])
+#     meta_df.update(kwd_metadata)
+#     return meta_df
+
+def add_kwds_to_df(kwds, merged_meta_df, kwd_keys=['Subject','HierarchicalSubject']):
+    #it would be faster to do this all at once rather than do once for each kwd_key, but it would take more janky code
+    kwds = kwds.rename('new_kwds')
+    for kwd_key in kwd_keys:
+        kwds_df = pd.DataFrame(kwds)
+        kwds_df['metadata_key'] = kwd_key
+        kwds_df = kwds_df.reset_index().set_index(['time_id','file_prefix','metadata_key'])
+        kwd_metadata = merged_meta_df.query(f"metadata_key == '{kwd_key}'").copy()
+        kwd_metadata = kwd_metadata.reset_index().set_index(['time_id','file_prefix','metadata_key']).join(kwds_df, how='right')
+        kwd_metadata['metadata_value'] = kwd_metadata.apply(extend_kwds, axis=1)
+        kwd_metadata['metadata_type'] = kwd_metadata['metadata_type'].fillna('synthetic')
+        kwd_metadata = kwd_metadata.reset_index().set_index(['time_id','file_prefix','metadata_type','metadata_key']).drop(columns='new_kwds')
+        merged_meta_df = pd.concat([merged_meta_df.loc[merged_meta_df.index.difference(kwd_metadata.index, sort=False)],kwd_metadata]) #upsert
+        #merged_meta_df.update(kwd_metadata)
+    return merged_meta_df
+# What filtering needs do I perceive?
+#    * any keyword
+#    * all keywords
+#    * better search? Probably worthless until captioning set up
+#    * Label (Only one that matters is Red)
+#    * Lens
+#    * Focal length range
+#    * other settings = or range
+#    * stars
+
+#for analysis I will want to make features from these, so maybe similar retrieval mechanism
+
+#Requires all filters to evaluate true. Might make better search if needed later
+def get_photos_by_metadata(filter_dictionary, merged_meta_df):
+    for key, criteria in filter_dictionary.items():
+        merged_meta_df.query(f'{key} {criteria}')
 
 
-def filter_by_metadata(filter_dictionary):
+def get_keepers(merged_meta_df):
     pass
+
+def find_patterns_in_keywords(kwd_list, pattern):
+    kwd_string = ' '.join(kwd_list)
+    kwd_found = bool(re.search(pattern,kwd_string))
+    return kwd_found
+
+def prepare_patterns(search_terms):
+    patterns = '|'.join([re.escape(term) for term in search_terms])
+    return patterns
+
+def get_unique_kws(df):
+    kws = df.query("metadata_key == 'Subject'")['metadata_value']
+    return set(kws.sum())
+    
+def fix_kw_fields(df, fields=['Subject', 'HierarchicalSubject']):
+    def field_kw_check(kw):
+        if kw != kw:
+            return []
+        if type(kw) == str:
+            kw = [kw]        
+
+    for field in fields:
+        df[field].apply(field_kw_check)
 
 # def make_jpg_meta_df(meta_df, meta_df_path, red_label_only=True):
 
@@ -282,7 +347,7 @@ if __name__ == '__main__':
     print(datetime.datetime.now())
 
     #reduce to dir with tags for testing
-    meta_df = meta_df.query("filepath.str.contains('2014-1H')")
+    #meta_df = meta_df.query("filepath.str.contains('2014-1H')")
 
     print("splitting")
     meta_df = split_filepath(meta_df)
@@ -292,10 +357,40 @@ if __name__ == '__main__':
     print(datetime.datetime.now())
     print("merging")
     merged_meta_df = create_merged_meta_df(meta_df)
-    meta_df = meta_df.set_index('time_id')
     print(datetime.datetime.now())
     print("tagging")
-    filepath_tags = convert_filepath_to_kws(meta_id_df, original_photo_dirs)
-    merged_meta_df = add_kws_to_df(filepath_tags, merged_meta_df)
+    filepath_tags, relevant_path_structure = convert_filepath_to_kwds(meta_id_df, original_photo_dirs)
+    merged_meta_df = add_kwds_to_df(filepath_tags, merged_meta_df)
 
+    
+    merged_meta_df = merged_meta_df.reset_index(level=['metadata_type','metadata_key'])    
+    
+    get_unique_kws(merged_meta_df)
+    merged_meta_df.columns
+    meta_df.file_prefix
+    
+    set(relevant_path_structure.new_kwds.apply(lambda x: '/'.join(x.strip('/').split('/')[:-1])))
+    
+    
+    kwds = ['Gansu','Chengdu']
+    kwds = prepare_patterns(kwds)
+    search_series = merged_meta_df.query("metadata_key == 'Subject'")['metadata_value']
+    kwd_results = search_series[search_series.apply(find_patterns_in_keywords, pattern=kwds)]
+    
+    
+    label_results = merged_meta_df[np.logical_and(merged_meta_df.metadata_key == 'Label', merged_meta_df.metadata_value == 'Red')]
+    
+    results = kwd_results.index.intersection(label_results.index)
+    
+    destination = '/media/zack/4055-128C/china_2014'
+    
+    files_to_fetch = meta_id_df.loc[results].reset_index('file_prefix')['file_prefix'].apply(lambda x: x +'.JPG')
+    for file in files_to_fetch:
+        shutil.copy2(file,destination)
+    
+    
     #print('here')
+    
+    kwds = filepath_tags
+   
+    kwd_key = 'Subject'
